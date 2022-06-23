2c5d7eed4d45389fac802e9212f21355564b71f0
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-06-23 16:38:44 +0100

    Updated tests

--- src/components/todos.test.js
+++ src/components/todos.test.js
@@ -9,11 +9,12 @@ test('Rendering Todos component and the text "test" is presnt', () => {
     expect(screen.getByText(/test/)).toBeInTheDocument();
 });
 
-test('Rendering Todos component and the text "test" is presnt', () => {
+test('Rendering Todos component and the text "test" and "bob" is presnt', () => {
 
-    let todos = [{ text:"test", toggleTodo: () => {}, key:0}, { text:"tester", toggleTodo: () => {}, key:0}]
+    let todos = [{ text:"test", toggleTodo: () => {}, key:0}, { text:"bob", toggleTodo: () => {}, key:1}]
     render(<Todos todos={todos} />)
     expect(screen.getByText(/test/)).toBeInTheDocument();
+    expect(screen.getByText(/bob/)).toBeInTheDocument();
 });
 
 test('renders checkbox', () => {
