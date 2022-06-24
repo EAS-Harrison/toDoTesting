6fb467594b10d5f8afab7d7485d057b6ff108fa5
EAS-Harrison <harrison@enterpriseautomation.co.uk>
2022-06-24 10:01:20 +0100

    .

--- src/components/todos.test.js
+++ src/components/todos.test.js
@@ -4,14 +4,14 @@ import Todos from './todos';
 
 test('Rendering Todos component and the text "test" is presnt', () => {
 
-    let todos = [{ text:"test", toggleTodo: () => {}, key:0}]
+    let todos = [{ text: "test", toggleTodo: () => { }, key: 0 }]
     render(<Todos todos={todos} />)
     expect(screen.getByText(/test/)).toBeInTheDocument();
 });
 
 test('Rendering Todos component and the text "test" and "bob" is presnt', () => {
 
-    let todos = [{ text:"test", toggleTodo: () => {}, key:0}, { text:"bob", toggleTodo: () => {}, key:1}]
+    let todos = [{ text: "test", toggleTodo: () => { }, key: 0 }, { text: "bob", toggleTodo: () => { }, key: 1 }]
     render(<Todos todos={todos} />)
     expect(screen.getByText(/test/)).toBeInTheDocument();
     expect(screen.getByText(/bob/)).toBeInTheDocument();
@@ -19,16 +19,7 @@ test('Rendering Todos component and the text "test" and "bob" is presnt', () => 
 
 test('renders checkbox', () => {
 
-    let todos = [{key:0}]
+    let todos = [{ key: 0 }]
     render(<Todos todos={todos} />)
     expect(screen.getByRole('checkbox').checked).toBeFalsy();
-
 });
-
-test('renders the same as the last Snapshot', () => {
-    let todos = [{ text:"test", toggleTodo: () => {}, key:0}]
-    const tree = renderer
-      .create(<Todos todos={todos} />)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
